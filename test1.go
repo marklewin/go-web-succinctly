34c88cadf6f64d8ef942413a7901e6c31538ed14
Mark Lewin <Mark Lewin>
2016-12-13 20:34:16 +0000

    test update from VScode

---
+++ test1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	myArray := [...]string{"Apples", "Oranges", "Bananas"}
+	fmt.Printf("Hello")
+	fmt.Printf("Initial array values: %v\n", myArray)
+	myFunction(myArray)
+	fmt.Printf("Final array values: %v\n", myArray)
+
+}
+
+func myFunction(arr [3]string) {
+	// Change Oranges to Strawberries
+	arr[1] = "Strawberries"
+	fmt.Printf("Array values in myFunction(): %v\n", arr)
+}
